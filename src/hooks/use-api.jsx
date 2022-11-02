721839f442a33a24ed74cc6f102c64885e9010b7
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-11-02 14:58:09 +0100

    .

--- src/hooks/use-api.jsx
+++ src/hooks/use-api.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import {useEffect, useState} from 'react';
+import {useAuth0} from '@auth0/auth0-react';
 import axios from "axios";
 
 export const useApi = (endpoint, options = {}) => {
-    const { getAccessTokenSilently } = useAuth0();
+    const {getAccessTokenSilently} = useAuth0();
     const [state, setState] = useState({
         error: null,
         loading: true,
@@ -13,7 +13,7 @@ export const useApi = (endpoint, options = {}) => {
     useEffect(() => {
         (async () => {
             try {
-                const { audience, scope, ...axiosParams } = options;
+                const {audience, scope, ...axiosParams} = options;
                 const accessToken = await getAccessTokenSilently();
                 const res = await axios("http://localhost:8080" + endpoint, {
                     ...axiosParams,
@@ -36,7 +36,9 @@ export const useApi = (endpoint, options = {}) => {
                 });
             }
         })();
-    }, []);
+        // Disabled rule because it's not correct. Source: https://stackoverflow.com/questions/68140108/react-hook-useeffect-has-missing-dependencies-xxx-either-include-them-or-remov
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [getAccessTokenSilently]);
 
     return {
         ...state
